b1917e4093d739ad389514be42ac29a5d8daafb2
LV <40906585+pschuongvtt@users.noreply.github.com>
2026-03-31 13:59:30 +0700

    Add files via upload

---
+++ Ch02/02_04/02_04b/react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+let language = "React";
+let moon = "🌙";
+
+function App() {
+  return (
+    <h1>
+      Hello {language.toUpperCase()} {moon}!!
+    </h1>
+  );
+}
+
+export default App;
